72ef2634113387e48fe115aab824104622f972f3
MatiSzkolaGH <smolarek.mateusz@szkola.elektrykplock.edu.pl>
2026-05-20 13:13:00 +0200

    structure of project

---
+++ FocusFlow/Models/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace FocusFlow.Models
+{
+    public class Tasks
+    {
+        public String Title { get; set; }
+        public bool TaskStatus { get; set; } = false;
+    }
+
+}
---
+++ FocusFlow/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using FocusFlow.Helpers;
+using FocusFlow.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusFlow.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private MainWindow mainWindow;
+
+        private ObservableCollection<Tasks> _tasksData;
+        public ObservableCollection<Tasks> TasksData
+        {
+            get { return _tasksData; }
+
+            set 
+            {
+                _tasksData = value; 
+                OnPropertyChanged();   
+            }
+        }
+
+        private TimeSpan _timer;
+        public TimeSpan Timer
+        {
+            get { return _timer; }
+            set
+            {
+                _timer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel(MainWindow mainWindow) { 
+            this.mainWindow = mainWindow;
+        }
+    }
+}
